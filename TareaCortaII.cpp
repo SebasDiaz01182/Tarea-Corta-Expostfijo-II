5d8b5ad27817667353e72dab603a54e2fbab3813
Sebastián Díaz Obando <sebascout01182@gmail.com>
2020-10-16 17:09:21 -0600

    Archivo listo para iniciar el proyecto

---
+++ TareaCortaII.cpp
@@ -0,0 +1,25 @@
+/* 
+Instituto Tecnologico de Costa Rica
+Estructura de datos
+Tarea Corta #2
+II Semestre 2020
+
+Luis Carlos Araya Mata
+Sebastián Díaz Obando
+*/
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
